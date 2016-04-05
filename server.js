b59d87e1582fd49b911f2eb3a63a8c129adf8a36
Joseph (Joe) Hill <joseph.hill@DFRFRM025072.local>
2016-04-04 20:49:42 -0400

    Connection to Mongo Getting There

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
 
-app.get('/', function (req, res) {
-  res.send("Hello World")
+
+
+var url = 'mongodb://localhost:27017/shortdb';
+
+// Use connect method to connect to the Server
+mongo.connect(url, function (err, db) {
+	if (err) throw err;
+
+	app.get('/', function (req, res) {
+		res.send("Hello World")
+	});
+    
+    app.get('/:animal', function(req, res){
+    	res.send("A " + req.params.animal);
+    });
+
+    //Close connection
+    //db.close();
+  
 });
+
+
 
 app.listen(process.env.PORT || 5000);
