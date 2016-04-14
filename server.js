7933910dfc2401f8c0b1db775b6a4c44b2a7d0ef
Joe Hill <joe@jkh3.com>
2016-04-13 20:47:01 -0400

    Allow CORS

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ mongo.connect(process.env.MONGOLAB_URI || url, function (err, db) {
 	if (err) throw err;
     var urlCollection = db.collection('urls');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 	app.get('/', function (req, res) {
 		res.sendFile(__dirname + "/index.html");
 	});
